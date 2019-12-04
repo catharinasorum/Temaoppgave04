38559d152f40e71fb6eb6c1066fd397b9911e64c
Catharina <catharina.sorum@outlook.com>
2019-12-04 18:46:30 +0100

    Updates

---
+++ precache-manifest.303cb84da83a16ac14f606dab7906966.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "323f99916b6476c0dbdafa9020f575be",
+    "url": "/Temaoppgave04/index.html"
+  },
+  {
+    "revision": "f074840df0460e216348",
+    "url": "/Temaoppgave04/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "d85d4a08cab8a6ac2e4d",
+    "url": "/Temaoppgave04/static/js/2.c90a5a83.chunk.js"
+  },
+  {
+    "revision": "f074840df0460e216348",
+    "url": "/Temaoppgave04/static/js/main.fe049313.chunk.js"
+  },
+  {
+    "revision": "72189efc0193a3029e37",
+    "url": "/Temaoppgave04/static/js/runtime-main.592d310c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Temaoppgave04/static/media/logo.25bf045c.svg"
+  }
+]);
