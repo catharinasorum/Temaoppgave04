ee876c1e2cd4fc0c74a68fd11ba9a76a07a68dd2
Catharina <catharina.sorum@outlook.com>
2019-12-04 18:52:10 +0100

    added files

---
+++ src/files/services.js
@@ -0,0 +1,35 @@
+const services = {
+    "description": "Services we provide",
+    "list": [
+        {
+            "treatment": "Haircut, long hair",
+            "price": "$15"
+        },
+        {
+            "treatment": "Haircut, short hair",
+            "price": "$12"
+        },
+        {
+            "treatment": "Haircut, barber",
+            "price": "$10"
+        },
+        {
+            "treatment": "Coloring, foils",
+            "price": "$30 and up"
+        },
+        {
+            "treatment": "Coloring, permanent",
+            "price": "$29 and up"
+        },
+        {
+            "treatment": "Basic styling",
+            "price": "$13"
+        },
+        {
+            "treatment": "Luxury styling",
+            "price": "$40 and up"
+        }
+    ]
+}
+
+export default services
